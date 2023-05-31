1b4c3a44af45a0b6e54620817bcee0a11c0c5526
ImGorapaduck <han@certifie.io>
2023-05-31 23:01:18 +0900

    design: 전역 폰트 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from "./logo.svg";
-import PayLoadCertificate_WB13_Screen from "./PayLoadCertificate_WB13_Screen";
+// import logo from "./logo.svg";
+// import PayLoadCertificate_WB13_Screen from "./PayLoadCertificate_WB13_Screen";
+import PayInputUserData_WB14_Screen from './PayInputUserData_WB14_Screen'
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <PayLoadCertificate_WB13_Screen />
+    <div className='App' >
+      {/* <PayLoadCertificate_WB13_Screen /> */}
+      <PayInputUserData_WB14_Screen />
     </div>
   );
 }
