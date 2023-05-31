c5719e381c875b2757e950f34d29eeee22fc4b56
ImGorapaduck <han@certifie.io>
2023-06-01 01:53:08 +0900

    feat: route 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 // import logo from "./logo.svg";
-// import PayLoadCertificate_WB13_Screen from "./PayLoadCertificate_WB13_Screen";
+import PayLoadCertificate_WB13_Screen from "./PayLoadCertificate_WB13_Screen";
 import PayInputUserData_WB14_Screen from './PayInputUserData_WB14_Screen'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './App.css'
 
 function App() {
   return (
-    <div className='App' >
-      {/* <PayLoadCertificate_WB13_Screen /> */}
-      <PayInputUserData_WB14_Screen />
-    </div>
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" Component={PayLoadCertificate_WB13_Screen} />
+          <Route path="/info" Component={PayInputUserData_WB14_Screen} />
+        </Routes >
+      </div>
+    </Router>
+
   );
 }
 
